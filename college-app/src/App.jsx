940244cf248349a1c5ad78990996c25e983e5484
Arslan Haider <arslanhaidermughal8@gmail.com>
2025-01-11 11:50:45 +0500

    Our program section is completed

--- college-app/src/App.jsx
+++ college-app/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from './Conponents/Navbar/Navbar'
 import Hero from './Conponents/Hero/Hero'
+import Programs from './Conponents/Programs/Programs'
+import Title from './Conponents/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subtitle='Our PROGRAM' title='What We Offer' />
+        <Programs />
+      </div>
+      
     </div>
   )
 }
